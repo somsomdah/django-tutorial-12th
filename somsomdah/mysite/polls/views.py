4fd9fd232d667711f029c1adba660f295def0565
somsomdah <somdah98@gmail.com>
2020-09-23 12:16:09 +0900

    change models

--- somsomdah/mysite/polls/views.py
+++ somsomdah/mysite/polls/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
 
-
+'''
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
@@ -28,36 +28,21 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-
 '''
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # template=loader.get_template('polls/index.html')
-    context = {"latest_question_list": latest_question_list}
-    return render(request, 'polls/index.html', context)  # polls/index.html에 context 전달
-    # return HttpResponse(template.render(context,request))
-    # return HttpResponse(output)
-    # return HttpResponse("Hello,world. You're at the polls index.")
+    return render(request, 'polls/index.html', {"latest_question_list": latest_question_list})  # polls/index.html에 context 전달
+
 
 
 def detail(request, question_id):
-    # try:
-    #    question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #    raise Http404('Question does not exists')
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
-    # return HttpResponse("You're looking at question %s" % question_id)
 
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
-    # response = "You're looking at the results of question %s"
-    # return HttpResponse(response % question_id)
-
-'''
 
 
 def vote(request, question_id):
